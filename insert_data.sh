c3f3ba6454603a9a40ccca66e23299a567aa6d73
prodri69 <127267670+prodri69@users.noreply.github.com>
2023-10-06 11:48:59 -0700

    Update insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -9,3 +9,9 @@ fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
 
+echo $($SPQL "TRUNCATE TABLE games, teams");
+
+cat games.csv| while IFS=',' read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+ do
+ echo $YEAR, $ROUND, $WINNER
+ done
